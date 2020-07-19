55f9d436011d9a30557bbedca51a370fa0e7149a
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-19 15:01:30 +0530

    added API for products and Backend is done

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,35 @@
 const Product = require('../../db').Product
 const route = require('express').Router();
 
+route.get('/' , (req , res) =>{
+        //Get all Products
+        Product.findAll()
+            .then((products)=>{
+                res.status(200).send(products)
+            })
+            .catch((e) => res.status(500).send({
+                error :`Cant retrive products: 
+                 ${e}`
+            }))
+})
+
+
+route.post('/' , (req , res) =>{
+//Add a new product 
+
+    if(isNaN(req.body.price)){
+        return res.status(403).send({
+            error : 'Price is not a valid number'
+        })
+    }
+
+    Product.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price : parseFloat(req.body.price)
+    })
+    .then((product)=> res.status(201).send({product}))
+    .catch((e)=> res.status(501).send({error : `Cant Add the product into the DB : ${e}`}))
+})
+
 exports = module.exports = route;
